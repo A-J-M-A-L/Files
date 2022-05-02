bbdea6d969a4c4ef88783a9036bbc31d677489cd
A-J-M-A-L <98047253+A-J-M-A-L@users.noreply.github.com>
2022-05-02 23:36:05 +0530

    Update bitly.js

--- plugins/bitly.js
+++ plugins/bitly.js
@@ -1,10 +1,10 @@
 let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
-  if (!text) throw 'url/link nya mana?'
+  if (!text) throw 'Where is the url/link?'
   let res = await fetch(global.API('xteam', '/shorturl/bitly', { url: text }, 'APIKEY'))
   let json = await res.json()
   if (json.status) m.reply(json.result.link)
-  else throw 'Link Invalid!\nPeriksa url anda'
+  else throw 'Link Invalid!\nPlease check the given url'
 }
 handler.help = ['bitly'].map(v => v + ' <link>')
 handler.tags = ['shortlink']
