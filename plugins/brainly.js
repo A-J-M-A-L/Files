6a3829ad9d6f64a93f4496818b3a9130c9ab9ee1
A-J-M-A-L <98047253+A-J-M-A-L@users.noreply.github.com>
2022-05-02 23:37:57 +0530

    Update brainly.js

--- plugins/brainly.js
+++ plugins/brainly.js
@@ -1,13 +1,13 @@
 let fetch = require('node-fetch')
 let handler = async (m, { text, command, usedPrefix }) => {
-    if (!text) throw `uhm.. teksnya mana?\n\ncontoh:\n${usedPrefix + command} membaca`
-    let res = await fetch(`https://api.xteam.xyz/brainly?soal=${text}&APIKEY=cristian9407`)
+    if (!text) throw `uhm.. where is the text?\n\nexample:\n${usedPrefix + command} read`
+    let res = await fetch(`https://api.xteam.xyz/brainly?about=${text}&APIKEY=cristian9407`)
     if (!res.ok) throw eror
     let json = await res.json()
     if (!json.status) throw json
     m.reply(json.jawaban)
 }
-handler.help = ['Brainly <teks>']
+handler.help = ['Brainly <text>']
 handler.tags = ['internet']
 handler.command = /^brainly$/i
 module.exports = handler
