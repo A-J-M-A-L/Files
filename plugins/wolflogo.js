1f70c61b1c873eed8140868bea58a3b2b8c68889
A-J-M-A-L <98047253+A-J-M-A-L@users.noreply.github.com>
2022-05-02 23:53:39 +0530

    Update wolflogo.js

--- plugins/wolflogo.js
+++ plugins/wolflogo.js
@@ -1,8 +1,8 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
    response = args.join(' ').split('|')
-  if (!args[0]) throw 'Masukkan Parameter'
-  m.reply('*[ ❗ ] Wait,Proses...*')
+  if (!args[0]) throw 'Enter Text'
+  m.reply('*Wait a minute⏳*\nprocessing ⚙️...')
   let res = `https://api.zeks.xyz/api/wolflogo?apikey=apivinz&text1=${response[0]}&text2=${response[1]}`
   conn.sendFile(m.chat, res, 'wolflogo.jpg', `Nih kak`, m, false)
 }
