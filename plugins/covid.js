3ef48dfcbc0db77e282526f2351b619ee37289ea
A-J-M-A-L <98047253+A-J-M-A-L@users.noreply.github.com>
2022-05-03 12:24:31 +0530

    Update covid.js

--- plugins/covid.js
+++ plugins/covid.js
@@ -3,18 +3,18 @@ let handler = async (m, { text }) => {
   let res = await fetch(global.API('https://covid19.mathdro.id', '/api/countries/'+ (text)))
   if (!res.ok) throw await res.text()
   let json = await res.json()
-  if (!json.confirmed) throw 'Negara?'
+  if (!json.confirmed) throw 'Country?'
   if (json.confirmed) m.reply(`
-Negara : ${text}
-Terkonfirmasi : ${json.confirmed.value}
-Sembuh : ${json.recovered.value}
-Meninggal : ${json.deaths.value}
+Country : ${text}
+Confirmed : ${json.confirmed.value}
+Recover : ${json.recovered.value}
+Died : ${json.deaths.value}
 Update Info : ${json.lastUpdate}
 `.trim())
   else throw json
 }
-handler.help = ['covid'].map(v => v + ' <negara>')
+handler.help = ['covid'].map(v => v + ' <country>')
 handler.tags = ['internet']
 handler.command = /^(corona|covid|covid19)$/i
-//susu, tetek, oppai
+//milk, tits, oppai
 module.exports = handler
