30ccf0a049d618480b1feb328351b9dbe3daedcc
A-J-M-A-L <98047253+A-J-M-A-L@users.noreply.github.com>
2022-05-03 15:46:09 +0530

    Update expired.js

--- plugins/expired.js
+++ plugins/expired.js
@@ -1,17 +1,17 @@
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0] || isNaN(args[0])) throw `Masukkan angka mewakili jumlah hari !\n*Misal : ${usedPrefix + command} 30*`
+    if (!args[0] || isNaN(args[0])) throw `Enter a number representing the number of days !\n*Example : ${usedPrefix + command} 30*`
 
     let who
     if (m.isGroup) who = args[1] ? args[1] : m.chat
     else who = args[1]
 
-    var jumlahHari = 86400000 * args[0]
+    var number ofDays = 86400000 * args[0]
     var now = new Date() * 1
-    if (now < global.db.data.chats[who].expired) global.db.data.chats[who].expired += jumlahHari
-    else global.db.data.chats[who].expired = now + jumlahHari
-    m.reply(`Berhasil menetapkan hari kadaluarsa untuk Grup ini selama ${args[0]} hari.\n\nHitung Mundur : ${msToDate(global.db.data.chats[who].expired - now)}`)
+    if (now < global.db.data.chats[who].expired) global.db.data.chats[who].expired += number of Days
+    else global.db.data.chats[who].expired = now + number ofDays
+    m.reply(`Successfully set expiration day for this Group for ${args[0]} days.\n\nCountdown : ${msToDate(global.db.data.chats[who].expired - now)}`)
 }
-handler.help = ['addsewa <hari>']
+handler.help = ['add rent <day>']
 handler.tags = ['owner']
 handler.command = /^(expired|addsewa)$/i
 handler.owner = true
@@ -26,6 +26,6 @@ function msToDate(ms) {
     minutes = Math.floor((hoursms) / (60 * 1000));
     minutesms = ms % (60 * 1000);
     sec = Math.floor((minutesms) / (1000));
-    return days + " hari " + hours + " jam " + minutes + " menit";
+    return days + " days " + hours + " hours " + minutes + " minutes";
     // +minutes+":"+sec;
 }
