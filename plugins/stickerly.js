b5f7eaf52d4fd5a32a21a0ecf21071432cee3173
A-J-M-A-L <98047253+A-J-M-A-L@users.noreply.github.com>
2022-05-03 21:10:21 +0530

    Update stickerly.js

--- plugins/stickerly.js
+++ plugins/stickerly.js
@@ -1,26 +1,26 @@
 const fetch = require('node-fetch')
 const { sticker5 } = require('../lib/sticker')
 
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) throw `Penggunaan:\n${usedPrefix + command} <teks>\n\nContoh:\n${usedPrefix + command} spongebob`
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+    if (!text) throw `Usage:\n${usedPrefix + command} <text>\n\nExample:\n${usedPrefix + command} spongebob`
     let res = await fetch(API('zeks', '/api/searchsticker', { q: text }, 'apikey'))
-    if (!res.ok) throw eror
+    if (!res.ok) throws error
     let json = await res.json()
     if (!json.status) throw json
-    let hasil = json.sticker.map((v, i) => `${i + 1}. ${v}`).join('\n')
+    let result = json.sticker.map((v, i) => `${i + 1}. ${v}`).join('\n')
     m.reply(`*${json.title}*
-*Estimasi selesai:* ${json.sticker.length * 1.5} detik
+*Estimated completion:* ${json.sticker.length * 1.5} seconds
 `.trim())
 
     for (let i of json.sticker) {
-        stiker = await sticker5(false, i, packname, author)
-        await conn.sendFile(m.chat, stiker, '', '', m, 0, { asSticker: true })
+        sticker = await sticker5(false, i, packname, author)
+        await conn.sendFile(m.chat, sticker, '', '', m, 0, { asSticker: true })
     }
-    m.reply('_*Selesai*_')
+    m.reply('_*Done*_')
 
 }
-handler.help = ['stikerly <teks>']
-handler.tags = ['sticker']
+handler.help = ['stickerly <text>']
+handler.tags = ['stickers']
 handler.command = /^(stic?kerly)$/i
 
 handler.limit = 3
