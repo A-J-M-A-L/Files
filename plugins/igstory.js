31bd51255d7031aafb5f8ccef471b8147251e041
A-J-M-A-L <98047253+A-J-M-A-L@users.noreply.github.com>
2022-05-03 19:41:44 +0530

    Update igstory.js

--- plugins/igstory.js
+++ plugins/igstory.js
@@ -2,15 +2,15 @@ const { igstory } = require('../lib/scrape')
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
-  if (!args[0]) throw `Pengunaan:\n${usedPrefix + command} <url>\n\nContoh:\n\n${usedPrefix + command} stikerinbot`
-  if (args[0].startsWith('http') || args[0].startsWith('@')) throw `username salah`
+  if (!args[0]) throw `Usage:\n${usedPrefix + command} <url>\n\nExample:\n\n${usedPrefix + command} toxic_ajmal_`
+  if (args[0].startsWith('http') || args[0].startsWith('@')) throw `incorrect username`
 
   igstory(args[0]).then(async res => {
     let igs = JSON.stringify(res)
     let json = JSON.parse(igs)
     await m.reply(global.wait)
     for (let { downloadUrl, type } of json)
-      conn.sendFile(m.chat, downloadUrl, 'ig' + (type == 'image' ? '.jpg' : '.mp4'), '*Nih*', m)
+      conn.sendFile(m.chat, downloadUrl, 'ig' + (type == 'image' ? '.jpg' : '.mp4'), '*Here*', m)
 
   })
 
