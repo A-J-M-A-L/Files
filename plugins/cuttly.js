ec3fc319a311ad11946bf53f6663880d02469ade
A-J-M-A-L <98047253+A-J-M-A-L@users.noreply.github.com>
2022-05-03 12:29:11 +0530

    UPDATED 🤠👌

--- plugins/cuttly.js
+++ plugins/cuttly.js
@@ -1,10 +1,10 @@
 let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
-  if (!text) throw 'url/link nya mana?'
+  if (!text) throw 'Where is the url/link?'
   let res = await fetch(`http://hadi-api.herokuapp.com/api/cuttly?url=${text}`)
   let json = await res.json()
   if (json.status) m.reply(json.result)
-  else throw 'Link Invalid!\nPeriksa url anda'
+  else throw 'Link Invalid!\nPlease check the given url'
 }
 handler.help = ['cuttly'].map(v => v + ' <link>')
 handler.tags = ['shortlink']
