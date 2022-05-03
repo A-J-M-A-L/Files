94b4eaf267b1f584dd0d61cd97d53927a67eca6d
A-J-M-A-L <98047253+A-J-M-A-L@users.noreply.github.com>
2022-05-03 21:08:49 +0530

    Update stickerfilter.js

--- plugins/stickerfilter.js
+++ plugins/stickerfilter.js
@@ -1,38 +1,42 @@
+// hyzerr official
 const uploadImage = require('../lib/uploadImage')
 const { sticker } = require('../lib/sticker')
-let fetch = require('node-fetch')
-const { MessageType } = require('@adiwajshing/baileys-md')
+const { MessageType } = require('@adiwajshing/baileys')
 const effects = ['greyscale', 'invert', 'brightness', 'threshold', 'sepia', 'red', 'green', 'blue', 'blurple', 'pixelate', 'blur']
 
-let handler = async (m, { conn, usedPrefix, text, command }) => {
+let handler = async (m, { conn, usedPrefix, text }) => {
   let effect = text.trim().toLowerCase()
   if (!effects.includes(effect)) throw `
-┌─「 *Daftar Efek* 」
-${effects.map(effect => `├ ${effect}`).join('\n')}
-└────
-Penggunaan:
-${usedPrefix + command} <efek>
-Contoh:
-${usedPrefix + command} greyscale
+*Usage:* ${usedPrefix}stickfilter <effectname>
+*Example:* ${usedPrefix}stickfilter invert
+
+*List Effect:*
+${effects.map(effect => `_> ${effect}_`).join('\n')}
 `.trim()
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw `Balas gambar dengan perintah *${usedPrefix + command}*`
-  if (!/image\/(jpe?g|png)/.test(mime)) throw `Media tidak didukung!`
+  if (!mime) throw 'No Image Found'
+  if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} not supported`
   let img = await q.download()
   let url = await uploadImage(img)
-  let apiUrl = API('https://some-random-api.ml/canvas/', encodeURIComponent(effect), {
+  let apiUrl = global.API('https://some-random-api.ml/canvas/', encodeURIComponent(effect), {
     avatar: url
   })
   try {
-    let stiker = await sticker(null, apiUrl, packname, author)
-    await conn.sendFile(m.chat, stiker, '', '', m, 0, { asSticker: true })
+    let stiker = await sticker(null, apiUrl, global.packname, global.author)
+    await conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+      quoted: m
+    })
   } catch (e) {
-    await conn.sendFile(m.chat, apiUrl, 'image.png', null, m, 0, { thumbnail: await (await fetch(apiUrl)).buffer() })
+    m.reply('Conversion to Sticker Failed, Sending as Image Instead')
+    await conn.sendFile(m.chat, apiUrl, 'image.png', null, m)
   }
 }
-handler.help = ['stikerfilter']
-handler.tags = ['sticker']
-handler.command = /^(s(tic?ker)?filter)$/i
 
+handler.help = ['stickfilter (caption|reply media)']
+handler.tags = ['sticker']
+handler.command = /^(stickfilter)$/i
+handler.limit = true
+handler.group = false
+handler.register = true
 module.exports = handler
