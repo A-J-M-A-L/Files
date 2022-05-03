06d2cef8427b1c5826996c0ca5c1703f3d19ef58
A-J-M-A-L <98047253+A-J-M-A-L@users.noreply.github.com>
2022-05-03 20:58:52 +0530

    Update sf.js

--- plugins/sf.js
+++ plugins/sf.js
@@ -1,11 +1,11 @@
 let handler = async (m, { text, usedPrefix, command }) => {
-    if (!text) throw `uhm.. teksnya mana?\n\npenggunaan:\n${usedPrefix + command} <teks>\n\ncontoh:\n${usedPrefix + command} plugins/melcanz.js`
-    if (!m.quoted.text) throw `balas pesan nya!`
+    if (!text) throw `uhm.. where is the text?\n\nuse:\n${usedPrefix + command} <text>\n\nexample:\n${usedPrefix + command} plugins/melcanz.js`
+    if (!m.quoted.text) throw `reply the message!`
     let path = `${text}`
     await require('fs').writeFileSync(path, m.quoted.text)
-    m.reply(`tersimpan di ${path}`)
+    m.reply(`stored in ${path}`)
 }
-handler.help = ['sf'].map(v => v + ' <teks>')
+handler.help = ['sf'].map(v => v + ' <text>')
 handler.tags = ['owner']
 handler.command = /^sf$/i
 
