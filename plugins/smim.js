1fd867905d055116d39afdb0bbdecd2080b61dec
A-J-M-A-L <98047253+A-J-M-A-L@users.noreply.github.com>
2022-05-03 21:06:26 +0530

    Update smim.js

--- plugins/smim.js
+++ plugins/smim.js
@@ -3,26 +3,26 @@ const { MessageType } = require('@adiwajshing/baileys-md')
 const { sticker } = require('../lib/sticker')
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-    let [atas, bawah] = text.split`|`
+    let [top, bottom] = text.split`|`
     let q = m.quoted ? m.quoted : m
     let mime = (q.msg || q).mimetype || ''
-    if (!mime) throw `balas gambar dengan perintah\n\n${usedPrefix + command} <${atas ? atas : 'teks atas'}>|<${bawah ? bawah : 'teks bawah'}>`
-    if (!/image\/(jpe?g|png)/.test(mime)) throw `_*Mime ${mime} tidak didukung!*_`
+    if (!mime) throw `reply image with command\n\n${usedPrefix + command} <${over ? top : 'top text'}>|<${bottom ? bottom : 'bottom text'}>`
+    if (!/image\/(jpe?g|png)/.test(mime)) throw `_*Mime ${mime} is not supported!*_`
     let img = await q.download()
     let url = await uploadImage(img)
-    meme = `https://api.memegen.link/images/custom/${encodeURIComponent(atas ? atas : '')}/${encodeURIComponent(bawah ? bawah : '')}.png?background=${url}`
+    meme = `https://api.memegen.link/images/custom/${encodeURIComponent(top ? top : '')}/${encodeURIComponent(bottom ? bottom : '')}.png?background=${url }`
 try {
-    let stiker = await sticker(null, meme, global.packname, global.author)
-    await conn.sendFile(m.chat, stiker, {
+    let sticker = await sticker(null, meme, global.packname, global.author)
+    await conn.sendFile(m.chat, sticker, {
       quoted: m
     })
-  } catch (e) {
-    m.reply('gagal membuat stiker, Mencoba Mengirim gambar')
-    await conn.sendFile(m.chat, meme, 'image.png', 'JADIKAN STIKER SECARA MANUAL DENGAN MENGETIK .S', m)
+  } catch(e) {
+    m.reply('failed to create sticker, Attempting to send image')
+    await conn.sendFile(m.chat, meme, 'image.png', 'MAKE A STICKER MANUALLY TYPING .S', m)
   }
 }
-handler.help = ['smim <teks atas>|<teks bawah>']
-handler.tags = ['sticker']
+handler.help = ['smim <top text>|<bottom text>']
+handler.tags = ['stickers']
 handler.command = /^(smim)$/i
 
 handler.limit = false
