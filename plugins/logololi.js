f9bed5fa2e55217d3725ce9fd4f6229bb57e7d79
A-J-M-A-L <98047253+A-J-M-A-L@users.noreply.github.com>
2022-05-03 20:22:42 +0530

    Update logololi.js

--- plugins/logololi.js
+++ plugins/logololi.js
@@ -1,13 +1,13 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
    response = args.join(' ').split('|')
-  if (!args[0]) throw 'Masukkan Parameter'
-  m.reply('*Wait Ngab*\nProses...')
+  if (!args[0]) throw 'Enter text1|text2\n\nexample: .logo3 Wizard|md'
+  m.reply('*Wait a minute⏳*\nprocessing ⚙️...')
   let res = `https://caliphapi.com/api/lolimaker?text=${response[0]}&text2=elyas&apikey=wVLiUzSa`
   conn.sendFile(m.chat, res, 'lolilogo.jpg', `Sudah Jadi`, m, false)
-}
-handler.help = ['logololi'].map(v => v + ' <text>')
+ }
+handler.help = ['logo2'].map(v => v + ' <text|text>')
 handler.tags = ['maker']
-handler.command = /^(logololi)$/i
+handler.command = /^(logo2)$/i
 
 module.exports = handler
