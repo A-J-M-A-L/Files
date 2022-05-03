d0a32005b0dd602019f797ea6510fa88f9e6362f
A-J-M-A-L <98047253+A-J-M-A-L@users.noreply.github.com>
2022-05-03 20:59:41 +0530

    Update sadboylogo.js

--- plugins/sadboylogo.js
+++ plugins/sadboylogo.js
@@ -1,13 +1,13 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
    response = args.join(' ').split('|')
-   if (!args[0]) return m.reply('masukkan text1 dan text2\n\nContok: .sadboylogo elyas|ganzz')
-   m.reply('*Wait Ngab*\nProses...')
+   if (!args[0]) return m.reply('Enter text1 dan text2\n\nExample: .sadlogo wizard|md')
+   m.reply('*Wait a minute⏳*\nprocessing ⚙️...')
   let res = `https://melcanz.com/sadboy?nama=${response[0]}&nama2=${response[1]}&apikey=dUtJxxvp`
   conn.sendFile(m.chat, res, 'sadboy.jpg', `Sudah Jadi`, m, false)
 }
-handler.help = ['sadboylogo'].map(v => v + ' <text|text>')
+handler.help = ['sadlogo'].map(v => v + ' <text|text>')
 handler.tags = ['maker']
-handler.command = /^(sadboylogo)$/i
+handler.command = /^(sadlogo)$/i
 
 module.exports = handler
