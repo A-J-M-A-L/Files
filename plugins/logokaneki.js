e925f0c41c6d0e7d31ed715c7c395808ea6fb0fc
A-J-M-A-L <98047253+A-J-M-A-L@users.noreply.github.com>
2022-05-03 20:19:45 +0530

    Update logokaneki.js

--- plugins/logokaneki.js
+++ plugins/logokaneki.js
@@ -1,14 +1,14 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
    response = args.join(' ')
-  if (!args[0]) throw 'Masukkan text\nContoh: .logokaneki elyas'
-  m.reply('*Wait ngab*\nProses...')
+  if (!args[0]) throw 'Enter text\nExample: .logo1 Wizard'
+  m.reply('*Wait a minute⏳*\nProcessing ⚙️...')
   let res = `https://caliphapi.com/api/kaneki?text=${response}&apikey=ELYASXD`
   conn.sendFile(m.chat, res, 'kaneki.jpg', `Sudah jadi`, m, false)
 }
-handler.help = ['logokaneki'].map(v => v + ' <teks>')
+handler.help = ['logo1'].map(v => v + ' <teks>')
 handler.tags = ['maker']
-handler.command = /^(logokaneki)$/i
+handler.command = /^(logo1)$/i
 handler.limit = true
 handler.register = false
 
