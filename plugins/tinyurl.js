986fd744bd2bf45ad4c20ba2d2e1c1182e22e6bc
A-J-M-A-L <98047253+A-J-M-A-L@users.noreply.github.com>
2022-05-03 21:15:16 +0530

    Update tinyurl.js

--- plugins/tinyurl.js
+++ plugins/tinyurl.js
@@ -3,7 +3,7 @@ let axios = require('axios')
 let fetch = require('node-fetch')
 
 let handler = async (m, { text, conn:fur, args }) => {
-  if (!text) throw 'url/link not forund?\n\n*EXAMPLE:*\n.tinyurl https://instagram.com'
+  if (!text) throw 'url/link not found?\n\n*EXAMPLE:*\n.tinyurl https://instagram.com'
 
 fur.reply(m.chat, await shortlink(text), m)
  
