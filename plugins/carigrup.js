408c503f8a40bf0d585badd9dcde6d870dcb548b
A-J-M-A-L <98047253+A-J-M-A-L@users.noreply.github.com>
2022-05-03 12:21:23 +0530

    Update carigrup.js

--- plugins/carigrup.js
+++ plugins/carigrup.js
@@ -1,13 +1,13 @@
 let fetch = require('node-fetch')
-let handler = async (m, { text, usedPrefix, command }) => {
-    if (!text) throw `uhm.. cari apa?\n\ncontoh:\n${usedPrefix + command} mabar`
+let handler = async(m, { text, usedPrefix, command }) => {
+    if (!text) throw `uhm.. what are you looking for?\n\nexample:\n${usedPrefix + command} mabar`
     let res = await carigroup(text, 'name')
-    if (!res.length) throw 'Group tidak ditemukan!'
-    let teks = res.map(res => res.subject + '\n' + res.link).join('\n────────────\n')
+    if (!res.length) throw 'Group not found!'
+    let text = res.map(res => res.subject + '\n' + res.link).join('\n────────────\n')
     let image = 'https://telegra.ph/file/f5ec51bac808f543ef1d7.png'
-    conn.sendButtonLoc(m.chat, await(await fetch(image)).buffer(),teks,wm,'Jangan Di Pencet', 'huuu', m)
+    conn.sendButtonLoc(m.chat, await(await fetch(image)).buffer(),text,wm,'Don't Push', 'huuu', m)
 }
-handler.help = ['carigrup <pencarian>']
+handler.help = ['carigroup <search>']
 handler.tags = ['tools']
 
 handler.command = /^carig(ro?up|c)/i
@@ -17,7 +17,7 @@ module.exports = handler
 const axios = require('axios')
 const cheerio = require('cheerio')
 async function carigroup(search, searchby = 'name') {
-    let { data } = await axios.get(global.API('http://ngarang.com', '/link-grup-wa/daftar-link-grup-wa.php', {
+    let { data } = await axios.get(global.API('http://ngarang.com', '/link-group-wa/list-link-group-wa.php', {
         search: encodeURIComponent(search),
         searchby,
     }))
