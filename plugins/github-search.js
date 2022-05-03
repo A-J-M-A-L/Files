41c9e2e21bba28d070695670806c7122f384a412
A-J-M-A-L <98047253+A-J-M-A-L@users.noreply.github.com>
2022-05-03 16:08:25 +0530

    Update github-search.js

--- plugins/github-search.js
+++ plugins/github-search.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch')
-let handler = async (m, { text }) => {
-    if (!text) throw 'Cari apa?'
+let handler = async(m, { text }) => {
+    if (!text) throw 'Search for what?'
     let res = await fetch(global.API('https://api.github.com', '/search/repositories', {
         q: text
     }))
@@ -10,17 +10,17 @@ let handler = async (m, { text }) => {
         return `
 ${1 + index}. *${repo.full_name}*${repo.fork ? ' (fork)' : ''}
 _${repo.html_url}_
-_Dibuat pada *${formatDate(repo.created_at)}*_
-_Terakhir update pada *${formatDate(repo.updated_at)}*_
-👁  ${repo.watchers}   🍴  ${repo.forks}   ⭐  ${repo.stargazers_count}
+_Created on *${formatDate(repo.created_at)}*_
+_Last update on *${formatDate(repo.updated_at)}*_
+${repo.watchers} ${repo.forks} ${repo.stargazers_count}
 ${repo.open_issues} Issue${repo.description ? `
-*Deskripsi:*\n${repo.description}` : ''}
+*Description:*\n${repo.description}` : ''}
 *Clone:* \`\`\`$ git clone ${repo.clone_url}\`\`\`
 `.trim()
     }).join('\n\n')
     m.reply(str)
 }
-handler.help = ['githubsearch'].map(v => v + ' <pencarian>')
+handler.help = ['githubsearch'].map(v => v + ' <search>')
 handler.tags = ['tools']
 
 handler.command = /^g(ithub|h)search$/i
@@ -30,12 +30,12 @@ module.exports = handler
 function formatDate(n, locale = 'id') {
     let d = new Date(n)
     return d.toLocaleDateString(locale, {
-      weekday: 'long',
+      weekdays: 'long',
       day: 'numeric',
-      month: 'long',
+      months: 'long',
       year: 'numeric',
       hour: 'numeric',
-      minute: 'numeric',
+      minutes: 'numeric',
       second: 'numeric'
     })
   }
